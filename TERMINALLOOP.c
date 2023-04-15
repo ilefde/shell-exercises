bfd09ef880735485cff30b3485e9f13b4bc3665e
ilefde <49443145+ilefde@users.noreply.github.com>
2023-04-15 21:45:27 +0800

    Create TERMINALLOOP.c

---
+++ TERMINALLOOP.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: prompt user to enter command.
+ */
+
+  while (int_mode)
+  {
+  int_mode = isatty(STDIN_FILENO);
+     if (int_mode == 1)
+     {
+       write(STDOUT_FILENO, “~$ “, 13);
+     }
