251372d8c74139d700d18a82eb16f0bb8bb84ce8
ilefde <49443145+ilefde@users.noreply.github.com>
2023-04-17 17:29:51 +0800

    Update TERMINALLOOP.c

--- TERMINALLOOP.c
+++ TERMINALLOOP.c
@@ -14,5 +14,6 @@ int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
      if (int_mode == 1)
      {
        write(STDOUT_FILENO, “~$ “, 13);
+       read(STDIN_FILENO)
      }
   }
